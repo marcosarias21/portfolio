b122b0e1e043a2869ac1d8295439e5a10a48c462
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-03 16:58:11 -0300

    home done

---
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import bgVideo from '../../assets/video/Letter - 18003.mp4';
+import './backgroundvideo.scss';
+
+const BackgroundVideo = () => {
+  return (
+    <video
+    autoPlay
+    muted loop
+    className='myVideo'
+    playsInline>
+      <source src={bgVideo} type='video/mp4' id='myVideo1' />
+    </video>
+  );
+};
+
+export default BackgroundVideo;
