f0fb0c20a3b865b7e9a7b8e56ccba2eeb46e028f
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-04 14:47:32 -0300

    little changes in main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import About from './pages/About/About';
 import Home from './pages/Home/Home';
 import InitialPage from './pages/InitialPage/InitialPage';
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path='' element={<InitialPage />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/about' element={<About />} />
     </Routes>
     </>
   );
---
+++ src/pages/About/About.js
@@ -0,0 +1,9 @@
+import Container from '../../components/Container/Container';
+
+const About = () => {
+  return (
+    <Container>Aboasdasdsaut</Container>
+  );
+};
+
+export default About;
