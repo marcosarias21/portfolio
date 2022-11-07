a0dd96e666a48d36cbd959d7529f6dbe35c72d88
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-07 12:31:59 -0300

    rename components with correct names and refactorize home page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,29 @@
+import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
 import Container from '../Container/Container';
-import WhoIm from '../WhoIm/WhoIm';
-import picture from '../../assets/images/me.jpg';
-import './about.scss';
+import ContainerMeImg from '../ContainerMeImg/ContainerMeImg';
+import Layout from '../Layout/Layout';
+import './About.scss';
 
 const About = () => {
   return (
-      <div className='bg-color-light'>
-        <Container>
-          <div className='container row align-items-center justify-content-center'>
-            <WhoIm />
-            <div className='text-center'>
-              <img className='img-size' src={picture} />
-            </div>
+    <Container>
+      <Layout>
+        <div className='col-sm-12 col-md-12 col-lg-7 text-center'>
+          <h2 className='fw-bold'> Who <span className='text-my-color'> I m?</span></h2>
+          {/* Primero muestra el y who i am en animacion y dsp´un delay para mostrar lo otro */}
+          <p className='text-style my-5'>Web developer apassionate who enjoys being challenged and participating in projects
+            that require me to work outside of my comfort zone to grow more and more each day </p>
+          <div>
+            <a href="https://github.com/marcosarias21" className='btn'> <AiFillGithub /></a>
+            <a href="https://www.linkedin.com/in/marcos-ar/" className='btn text-primary'> <FaLinkedinIn /></a>
+            <button className='btn btn-outline-primary '> Download CV </button>
           </div>
-        </Container>
-      </div>
+        </div>
+        <ContainerMeImg />
+      </Layout>
+    </Container>
   );
 };
 
--- src/components/WhoIm/index.js
+++
@@ -1,1 +0,0 @@
-export { default as WhoIm } from './WhoIm';
