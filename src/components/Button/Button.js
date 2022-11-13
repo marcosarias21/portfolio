81842367ca41126d48f27d75bd378e20fd33c757
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-13 18:58:31 -0300

    Reusable button in hero component and contact form and add style for dark mode in the form

---
+++ src/components/Button/Button.js
@@ -0,0 +1,24 @@
+import { motion } from 'framer-motion';
+import './button.scss';
+
+const Button = ({ text }) => {
+  return (
+    <motion.button
+      initial={{ opacity: 0, x: -200 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ delay: 1.2, duration: 0.5 }}
+      className='button-animated mt-4 py-3 px-4'>
+      <div className="svg-wrapper-1" >
+        <div className="svg-wrapper">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+          </svg>
+        </div>
+      </div>
+      <span className='fw-bold'>{text}</span>
+    </motion.button>
+  );
+};
+
+export default Button;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,6 +8,7 @@ import imgLight from '../../assets/images/imgLight.gif';
 import imgDark from '../../assets/images/imageDark.gif';
 import './hero.scss';
 import { useThemeContext } from '../../provider/ThemeContext';
+import { Button } from '../Button';
 
 const Hero = () => {
   const theme = useThemeContext();
@@ -35,21 +36,7 @@ const Hero = () => {
                 loop: true,
               }}
             />
-            <motion.button
-              initial={{ opacity: 0, x: -200 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: 1.2, duration: 0.5 }}
-              className='button-animated mt-4 py-3 px-4'>
-              <div className="svg-wrapper-1" >
-                <div className="svg-wrapper">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-                  </svg>
-                </div>
-              </div>
-              <span className='fw-bold'>Contact Me</span>
-            </motion.button>
+            <Button text='Contact Me' />
           </div>
           <Images className='img-media-size' photo={ theme === 'light' ? imgLight : imgDark }/>
       </Layout>
