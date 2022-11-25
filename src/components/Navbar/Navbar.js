ce6e960e6138fcd4421490b6cd835ca898103e04
marcosarias21 <marcosariasolaiz@gmail.com>
2022-11-25 14:56:12 -0300

    navbar show when is hover

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+import { useState } from 'react';
 import {
   BsStars, BsFillHouseDoorFill, BsCast, BsChat,
 } from 'react-icons/bs';
@@ -8,11 +10,11 @@ import './navbar.scss';
 const Navbar = () => {
   return (
     <header>
-      <nav className="d-flex flex-column justify-content-between">
+      <nav className='d-flex flex-column justify-content-between menu-hidden'>
         <div>
           <h1 className='fw-bold'>hola</h1>
         </div>
-        <div className='nav-item d-flex flex-column align-items-center'>
+        <div className='nav-item d-flex flex-column'>
           <a className="nav-link" aria-current="page" href="#"><BsFillHouseDoorFill className='mb-1' /><span>Home</span></a>
           <a className="nav-link" href="#"><FaGithub /><span>About</span></a>
           <a className="nav-link" href="#"><BsStars className='mb-1' /><span>Skills</span></a>
